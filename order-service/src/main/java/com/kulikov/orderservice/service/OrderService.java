a4462c26ce742c7668498db28ee179e0bafc6d27
holdmelol <ineedabraintoo@gmail.com>
2023-07-31 19:39:02 +0300

    adding kafka

--- order-service/src/main/java/com/kulikov/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/kulikov/orderservice/service/OrderService.java
@@ -3,10 +3,14 @@ package com.kulikov.orderservice.service;
 import com.kulikov.orderservice.dto.InventoryResponse;
 import com.kulikov.orderservice.dto.OrderLineItemsDto;
 import com.kulikov.orderservice.dto.OrderRequest;
+import com.kulikov.orderservice.event.OrderPlacedEvent;
 import com.kulikov.orderservice.model.Order;
 import com.kulikov.orderservice.model.OrderLineItems;
 import com.kulikov.orderservice.repository.OrderRepository;
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -20,6 +24,8 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webclientBuilder;
+    private final Tracer tracer;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -37,24 +43,29 @@ public class OrderService {
                 .toList();
 
 
-        InventoryResponse[] inventoryResponses =  webclientBuilder.build().get()
-                .uri("http://inventory-service/api/inventory",
-                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
-                .retrieve()
-                .bodyToMono(InventoryResponse[].class)
-                .block();
+        Span invServiceLookup = tracer.nextSpan().name("lookup");
+        try(Tracer.SpanInScope spanInScope = tracer.withSpan(invServiceLookup.start())) {
+            InventoryResponse[] inventoryResponses =  webclientBuilder.build().get()
+                    .uri("http://inventory-service/api/inventory",
+                            uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class)
+                    .block();
 
-        List<InventoryResponse> notInStockProducts = Arrays.stream(inventoryResponses)
-                .filter(inventoryResponse -> !inventoryResponse.isInStock())
-                .toList();
+            List<InventoryResponse> notInStockProducts = Arrays.stream(inventoryResponses)
+                    .filter(inventoryResponse -> !inventoryResponse.isInStock())
+                    .toList();
 
-        if (notInStockProducts.isEmpty()) {
-            orderRepository.save(order);
-            return "Order placed";
-        } else {
-            throw new IllegalArgumentException("Products is not in stock: " + notInStockProducts);
+            if (notInStockProducts.isEmpty()) {
+                orderRepository.save(order);
+                kafkaTemplate.send("notification", new OrderPlacedEvent(order.getOrderNumber()));
+                return "Order placed";
+            } else {
+                throw new IllegalArgumentException("Products is not in stock: " + notInStockProducts);
+            }
+        } finally {
+            invServiceLookup.end();
         }
-
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
